d2d8adf6a3c2faaf6dce8eaedf83329c5e322d07
Numan <33553832+Numi2@users.noreply.github.com>
2026-08-06 19:20:16 +0200

    Add hyper-policy native compiler unit

---
+++ src/core/HyperPolicyProgram.cpp
@@ -0,0 +1,4 @@
+#include "hyper_policy/HyperPolicyProgram.00.inc"
+#include "hyper_policy/HyperPolicyProgram.01.inc"
+#include "hyper_policy/HyperPolicyProgram.02.inc"
+#include "hyper_policy/HyperPolicyProgram.03.inc"
